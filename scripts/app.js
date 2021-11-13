e742088c8929ab57f74d931aeeabdb162a727282
Robert Hardy <rhardy@dcsdk12.org>
2021-11-13 10:00:49 -0700

    Add parabolic easing function

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,45 @@
+// @ts-check
 /** @type {HTMLCanvasElement} */
+// @ts-ignore we know canvas is indeed an HTMLCanvasElement
 const canvas = document.getElementById("game-canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 600;
 
+/**  @param {number} t */
+function parabolicEasing(t) {
+	// given a parabolla with x intercepts of 0 and 4
+	// and a y vertex of 4 we have a formula of:
+	// y = -(x - 2)^2 + 4
+	const x = t * 4 - 2;
+	const y = x * x * -1 + 4;
+
+	// translate y back into a percentage from 0 to 1+
+	return y / 4;
+}
+
 class Player {
 	constructor() {
-		this.x = canvas.width * 0.25;
-		this.y = 0;
-		this.speed = 10;
 		this.radius = 16;
+		this.maxBounceHeight = 100;
+		this.yLastBounce = 300;
+		this.x = canvas.width * 0.25;
+		this.y = this.yLastBounce;
+		this.timeSinceBounce = 0;
+		this.maxBounceTime = 1000;
 	}
 
-	update() {
-		this.y = this.y + this.speed;
+	/** @param {number} timeElapsed	*/
+	update(timeElapsed) {
+		this.timeSinceBounce += timeElapsed;
+		const isMovingUp = this.timeSinceBounce <= this.maxBounceTime / 2;
+		if (!isMovingUp && this.y >= canvas.height - this.radius) {
+			this.yLastBounce = canvas.height - this.radius;
+			this.timeSinceBounce = 0;
+		}
+
+		let ef = parabolicEasing(this.timeSinceBounce / this.maxBounceTime);
+		this.y = this.yLastBounce - ef * this.maxBounceHeight;
 	}
 
 	render() {
@@ -26,10 +52,15 @@ class Player {
 }
 
 let player = new Player();
+let lastTimestamp = 0;
 
-function gameLoop() {
+/** @param {number} timestamp */
+function gameLoop(timestamp) {
+	const timeElapsed = timestamp - lastTimestamp;
+	lastTimestamp = timestamp;
+
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	player.update();
+	player.update(timeElapsed);
 	player.render();
 
 	requestAnimationFrame(gameLoop);
